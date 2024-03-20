a73a31f014d9548f3ef81b341a919688507194a9
kunlangg <kunlangr@gmail.com>
2024-03-20 11:29:08 +0700

    save data

---
+++ app/src/main/java/com/example/datateman/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.datateman
+
+import android.content.Intent
+import android.media.MediaPlayer.OnCompletionListener
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.Toast
+import com.example.datateman.databinding.ActivityLoginBinding
+import com.example.datateman.databinding.ActivityMainBinding
+import com.firebase.ui.auth.AuthUI
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private var auth:FirebaseAuth? = null
+    private val RC_SIGN_IN = 1
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.logout.setOnClickListener(this)
+        binding.save.setOnClickListener(this)
+        binding.showData.setOnClickListener(this)
+        auth = FirebaseAuth.getInstance()
+    }
+
+    private fun isEmpty(s: String) : Boolean {
+        return TextUtils.isEmpty(s)
+    }
+
+    override fun onClick(p0: View?) {
+        when (p0?.getId()) {
+            R.id.save -> {
+                val getUserID = auth!!.currentUser!!.uid
+                val database = FirebaseDatabase.getInstance()
+
+                val getNama: String = binding.nama.getText().toString()
+                val getAlamat: String = binding.alamat.getText().toString()
+                val getNoHp: String = binding.noHp.getText().toString()
+
+                val getReference: DatabaseReference
+                getReference = database.reference
+
+                if (isEmpty(getNama) || isEmpty(getAlamat) || isEmpty(getNoHp)) {
+                    Toast.makeText(this@MainActivity, "Data tidak boleh ada yag kosong",Toast.LENGTH_SHORT).show()
+                } else {
+                    getReference.child("Admin").child(getUserID).child("DataTeman").push()
+                        .setValue(data_teman(getNama, getAlamat, getNoHp))
+                        .addOnCompleteListener(this){
+                            binding.nama.setText("")
+                            binding.alamat.setText("")
+                            binding.noHp.setText("")
+                            Toast.makeText(this@MainActivity, "Data Tersimpan", Toast.LENGTH_SHORT).show()
+                        }
+                }
+            }
+            R.id.logout -> {
+                AuthUI.getInstance().signOut(this)
+                    .addOnCompleteListener(object : OnCompleteListener<Void> {
+                        override fun onComplete(p0: Task<Void>) {
+                            Toast.makeText(this@MainActivity, "Logout Berhasil", Toast.LENGTH_SHORT)
+                                .show()
+                            intent = Intent(applicationContext, LoginActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        }
+                    })
+            }
+            R.id.show_data -> {}
+        }
+        }
+}
